a8a5bfad0ad2ce870fa7da409f07829ff6eeadf7
Clint Harvey <sch@rw-dev.com>
2017-06-14 12:21:25 -0500

    Third test
    Verifying that a start time the next morning (prior to 4am) is valid.
    
    Also refactored tests to make our startTime variable global so that
    it’s not defined for every test.

--- src/__tests__/BabySitterBooking.test.js
+++ src/__tests__/BabySitterBooking.test.js
@@ -4,11 +4,12 @@ import Moment from 'moment';
 import BabySitterBooking from '../BabySitterBooking';
 
 const babySitterBooking = BabySitterBooking();
+let startTime = null;
 
 describe('BabysitterBooking startTime ', () => {
 
   it('returns a 400 error if startTime is earlier than 5:00PM', () => {
-    const startTime = new Moment().startOf('day').hour(16).minute(30).format('X');
+    startTime = new Moment().startOf('day').hour(16).minute(30).format('X');
     let bookingReturn = babySitterBooking.validateBooking(startTime);
 
     expect(bookingReturn.code).toEqual(400);
@@ -16,7 +17,7 @@ describe('BabysitterBooking startTime ', () => {
   });
 
   it('returns a 200 OK if startTime is between than 5:00PM and 4:00AM tomorrow', () => {
-    const startTime = new Moment().startOf('day').hour(17).minute(30).format('X');
+    startTime = new Moment().startOf('day').hour(17).minute(30).format('X');
     let bookingReturn = babySitterBooking.validateBooking(startTime);
 
     expect(bookingReturn.code).toEqual(200);
@@ -24,10 +25,19 @@ describe('BabysitterBooking startTime ', () => {
   });
 
   it('returns a 200 OK if startTime is exactly 5:00PM', () => {
-      const startTime = new Moment().startOf('day').hour(17).minute(0).format('X');
+      startTime = new Moment().startOf('day').hour(17).minute(0).format('X');
       let bookingReturn = babySitterBooking.validateBooking(startTime);
 
       expect(bookingReturn.code).toEqual(200);
       expect(bookingReturn.message).toEqual('OK');
     });
+
+  it('returns a 200 OK if startTime is the next morning before 4am', () => {
+    startTime = new Moment().startOf('day').hour(26).minute(30).format('X');
+    let bookingReturn = babySitterBooking.validateBooking(startTime);
+
+    expect(bookingReturn.code).toEqual(200);
+    expect(bookingReturn.message).toEqual('OK');
+  });
+
 });
