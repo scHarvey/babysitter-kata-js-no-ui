830ef4648de4fea21587c003b19b2c72b8b65b83
Clint Harvey <sch@rw-dev.com>
2017-06-14 12:33:37 -0500

    Fifth test
    Adding an end time and verifying that start time is not after end time.
    
    Unrelated: I mislabeled my last commit as test three it was actually
    test four

--- src/BabySitterBooking.js
+++ src/BabySitterBooking.js
@@ -11,8 +11,13 @@ export default () => {
 
   let response = {};
   return {
-    validateBooking(startTime) {
-      if (Moment.unix(startTime).isBefore(_earliestStartTime)) {
+    validateBooking(startTime = _earliestStartTime, endTime = _latestEndTime) {
+      if (Moment.unix(startTime).isAfter(Moment.unix(endTime))) {
+        response = {
+          code: 400,
+          message: 'Start Time can not be later than End Time.'
+        };
+      } else if (Moment.unix(startTime).isBefore(_earliestStartTime)) {
         response = {
           code: 400,
           message: 'Start time is earlier than the allowed time.'
--- src/__tests__/BabySitterBooking.test.js
+++ src/__tests__/BabySitterBooking.test.js
@@ -4,13 +4,22 @@ import Moment from 'moment';
 import BabySitterBooking from '../BabySitterBooking';
 
 const babySitterBooking = BabySitterBooking();
+let bookingReturn = null;
 let startTime = null;
+let endTime = null;
+
+beforeEach(() => {
+  // reset testing vars
+  bookingReturn = null;
+  startTime = null;
+  endTime = null;
+});
 
 describe('BabysitterBooking startTime ', () => {
 
   it('returns a 400 error if startTime is earlier than 5:00PM', () => {
     startTime = new Moment().startOf('day').hour(16).minute(30).format('X');
-    let bookingReturn = babySitterBooking.validateBooking(startTime);
+    bookingReturn = babySitterBooking.validateBooking(startTime);
 
     expect(bookingReturn.code).toEqual(400);
     expect(bookingReturn.message).toEqual('Start time is earlier than the allowed time.');
@@ -18,7 +27,7 @@ describe('BabysitterBooking startTime ', () => {
 
   it('returns a 200 OK if startTime is between than 5:00PM and 4:00AM tomorrow', () => {
     startTime = new Moment().startOf('day').hour(17).minute(30).format('X');
-    let bookingReturn = babySitterBooking.validateBooking(startTime);
+    bookingReturn = babySitterBooking.validateBooking(startTime);
 
     expect(bookingReturn.code).toEqual(200);
     expect(bookingReturn.message).toEqual('OK');
@@ -26,7 +35,7 @@ describe('BabysitterBooking startTime ', () => {
 
   it('returns a 200 OK if startTime is exactly 5:00PM', () => {
       startTime = new Moment().startOf('day').hour(17).minute(0).format('X');
-      let bookingReturn = babySitterBooking.validateBooking(startTime);
+      bookingReturn = babySitterBooking.validateBooking(startTime);
 
       expect(bookingReturn.code).toEqual(200);
       expect(bookingReturn.message).toEqual('OK');
@@ -34,10 +43,19 @@ describe('BabysitterBooking startTime ', () => {
 
   it('returns a 200 OK if startTime is the next morning before 4am', () => {
     startTime = new Moment().startOf('day').hour(26).minute(30).format('X');
-    let bookingReturn = babySitterBooking.validateBooking(startTime);
+    bookingReturn = babySitterBooking.validateBooking(startTime);
 
     expect(bookingReturn.code).toEqual(200);
     expect(bookingReturn.message).toEqual('OK');
+  });
+
+  it('returns a 400 if startTime is after endTime', () => {
+    startTime = new Moment().startOf('day').hour(19).minute(30).format('X');
+    endTime = new Moment().startOf('day').hour(19).minute(0).format('X');
+    bookingReturn = babySitterBooking.validateBooking(startTime, endTime);
+
+    expect(bookingReturn.code).toEqual(400);
+    expect(bookingReturn.message).toEqual('Start Time can not be later than End Time.');
   });
 
 });
