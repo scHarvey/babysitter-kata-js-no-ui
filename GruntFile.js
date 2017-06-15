100b33b18ae42fe0538e11428b72cd106fe5ff47
Clint Harvey <sch@rw-dev.com>
2017-06-15 14:20:48 -0500

    Final devops: was having issues with build

--- GruntFile.js
+++ GruntFile.js
@@ -2,26 +2,30 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    clean: ['temp', 'build'],
+    clean: ['tmp/**/*', '!tmp/.gitkeep', 'build/**/*', '!build/.gitkeep'],
+    babel: {
+      options: {
+        sourceMap: true,
+        presets: ['es2015']
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: ['**/*.js', '!__tests__/**/*.js'],
+          dest: 'tmp',
+          ext: '.js'
+        }]
+      }
+    },
     concat: {
       options: {
         separator: ';'
       },
       dist: {
-        src: ['src/**/*.js', '!src/__tests__/**/*.js'],
-        dest: 'tmp/<%= pkg.name %>.js'
+        src: ['tmp/**/*.js', '!tmp/__tests__/**/*.js'],
+        dest: 'build/<%= pkg.name %>.js'
       }
-    },
-    babel: {
-  		options: {
-  			sourceMap: true,
-  			presets: ['es2015']
-  		},
-  		dist: {
-  			files: {
-  				'build/<%= pkg.name %>.js': 'tmp/<%= pkg.name %>.js'
-  			}
-  		}
     },
     uglify: {
       options: {
@@ -34,11 +38,10 @@ module.exports = function(grunt) {
         }
       }
     }
-
   });
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-babel');
-  grunt.registerTask('default', ['clean', 'concat', 'babel', 'uglify']);
+  grunt.registerTask('default', ['clean', 'babel', 'concat', 'uglify']);
 }
